dee69c8fbd04c45c96d414c8cdea400ff9d18581
AFE-GmdG <afe-gmdg@gmx.de>
2022-06-23 13:19:46 +0200

    find no second % char fixed

--- redirect.js
+++ redirect.js
@@ -5,12 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const state = parts.find((part) => part.startsWith("state"));
     const redirectUriIndex = state.indexOf("%3d%7c");
     const redirectUriBase64 = state.substring(redirectUriIndex + 6);
-    const redirectUriBase64Length = redirectUriBase64.indexOf("%");
-    if (redirectUriBase64Length > 0) {
-      const redirectUri = atob(redirectUriBase64.substring(0, redirectUriBase64Length));
-      window.location.replace(redirectUri + hash);
-      return;
-    }
+    const redirectUriBase64Length = redirectUriBase64.indexOf("%") >= 0 ? redirectUriBase64.indexOf("%") : redirectUriBase64.length;
+    const redirectUri = atob(redirectUriBase64.substring(0, redirectUriBase64Length));
+    window.location.replace(redirectUri + hash);
+    return;
   } catch {
   }
   app.append(document.createTextNode("Error redirecting..."));
