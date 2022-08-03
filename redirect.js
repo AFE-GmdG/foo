1490f16716431af5913057e98116d1bd7dda288b
AFE-GmdG <afe-gmdg@gmx.de>
2022-08-03 10:59:43 +0200

    improve logout behavior

--- redirect.js
+++ redirect.js
@@ -13,10 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     if (search) {
       const parts = search.split("&");
-      const state = parts.find((part) => part.startsWith("state"));
-      const redirectUriBase64 = state.substring(6);
-      const redirectUriBase64Length = redirectUriBase64.indexOf("%") >= 0 ? redirectUriBase64.indexOf("%") : redirectUriBase64.length;
-      const redirectUri = atob(redirectUriBase64.substring(0, redirectUriBase64Length));
+      const redirectUri = parts.reduce((acc, part) => {
+        if (acc) {
+          return acc;
+        }
+        const matches = part.match(/^(?:\?)?(?:state=)(.+)$/i);
+        if (!matches) {
+          return null;
+        }
+        const state = decodeURIComponent(matches[1]);
+        return atob(state);
+      }, null);
       window.location.replace(redirectUri + search);
     }
   } catch {
