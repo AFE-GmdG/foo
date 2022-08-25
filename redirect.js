04668b5f2b7bb6720bc299d8d0b6f842391e6548
AFE-GmdG <afe-gmdg@gmx.de>
2022-08-25 08:44:54 +0200

    Minor usage improvement
    - Adding a return to not show an error if a redirect is possible
    - Cut away the search from the logout situation
    - Adding comments

--- redirect.js
+++ redirect.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   try {
     const { hash, search } = location;
     if (hash) {
+      // Used at login: Extract the redirect URI from the hash and redirect to it.
+      // Add the original hash to the redirect URI; it's needed to get the token.
       const parts = hash.split("&");
       const state = parts.find((part) => part.startsWith("state"));
       const redirectUriIndex = state.indexOf("%3d%7c");
@@ -12,6 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
     if (search) {
+      // Used at logout: Extract the logout redirect URI from the query string and redirect to it.
+      // Do not add the original query string to the redirect URI.
       const parts = search.split("&");
       const redirectUri = parts.reduce((acc, part) => {
         if (acc) {
@@ -24,9 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const state = decodeURIComponent(matches[1]);
         return atob(state);
       }, null);
-      window.location.replace(redirectUri + search);
+
+      window.location.replace(redirectUri);
+      return;
     }
   } catch {
+    // Ignore any errors.
   }
+  // In case of any error or not hash or search is present, just add a message to the page.
   app.append(document.createTextNode("Error redirecting..."));
 });
